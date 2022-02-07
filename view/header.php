f1db9529e8996cb9535ffc98d60a16146d69713f
stephane roy <stephroy411@hotmail.com>
2022-02-07 18:29:04 -0400

    Commit from windows OS project(xampp)

---
+++ view/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment Tracker</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;1,200&display=swap" rel="stylesheet"> 
+    <Link rel="stylesheet" href="view/css/main.min.css" />
+</head>
+
+<body>
+    <main class="main">
